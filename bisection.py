381ac185c579e7413f1e5bd5203e43f5c544ecb8
arthur <arthur.teisseire@epitech.eu>
2018-01-12 19:35:30 +0100

    0 is now excluded from the condition and the last print is deleted : truncate should resolve the problem

--- bisection.py
+++ bisection.py
@@ -8,12 +8,10 @@ def bisection(a0, a1, a2, a3, a4, n):
 		print('{x_n:.{n}g}'.format(n = n, x_n = x_n))
 		y_d = f4(x_d, a0, a1, a2, a3, a4)
 		y_f = f4(x_n, a0, a1, a2, a3, a4)
-		if ((y_d >= 0 and y_f <= 0) or (y_d <= 0 and y_f >= 0)):
+		if ((y_d > 0 and y_f < 0) or (y_d < 0 and y_f > 0)):
 			x_f = x_n
 		else:
 			x_d = x_n
-	x_n = (x_d + x_f) / 2
-	print('{:.6g}'.format(x_n))
 
 def f4(x, a0, a1, a2, a3, a4):
 	return (a4 * pow(x, 4) + a3 * pow(x, 3) + a2 * pow(x, 2) + a1 * x + a0)
