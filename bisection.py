3879417e58d7b52a85cd794bdff30fd5107a2ffc
arthur <arthur.teisseire@epitech.eu>
2018-01-13 23:14:09 +0100

    bisection works as expected with the good formated string

--- bisection.py
+++ bisection.py
@@ -1,17 +1,15 @@
 def bisection(a0, a1, a2, a3, a4, n):
 	x_d = 0.0
 	x_f = 1.0
-	y_d = 0.0
-	y_f = 1.0
-	while(y_f - y_d > pow(10, -n)):
+	while (x_f - x_d > pow(10, -n)):
 		x_n = (x_d + x_f) / 2
-		print('{x_n:.{n}g}'.format(n = n, x_n = x_n))
+		print('%s' %(str(x_n).rstrip('0')[:n + 2]))
 		y_d = f4(x_d, a0, a1, a2, a3, a4)
 		y_f = f4(x_n, a0, a1, a2, a3, a4)
-		if (y_f * y_d <= 0):
-			x_f = x_n
-		else:
+		if (y_f * y_d > 0):
 			x_d = x_n
+		else:
+			x_f = x_n
 
 def f4(x, a0, a1, a2, a3, a4):
 	return (a4 * pow(x, 4) + a3 * pow(x, 3) + a2 * pow(x, 2) + a1 * x + a0)
