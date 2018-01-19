ffa50395d8619dece37a078d3025d4c42e55486f
arthur <arthur.teisseire@epitech.eu>
2018-01-19 16:50:22 +0100

    indent reajusted

--- bisection.py
+++ bisection.py
@@ -1,15 +1,15 @@
 def bisection(a0, a1, a2, a3, a4, n):
-	x_d = 0.0
-	x_f = 1.0
-	while (x_f - x_d > pow(10, -n)):
-		x_n = (x_d + x_f) / 2
-		print('x = %s' %(str(x_n).rstrip('0')[:n + 2]))
-		y_d = f4(x_d, a0, a1, a2, a3, a4)
-		y_f = f4(x_n, a0, a1, a2, a3, a4)
-		if (y_f * y_d > 0):
-			x_d = x_n
-		else:
-			x_f = x_n
+    x_d = 0.0
+    x_f = 1.0
+    while (x_f - x_d > pow(10, -n)):
+        x_n = (x_d + x_f) / 2
+        print('x = %s' %(str(x_n).rstrip('0')[:n + 2]))
+        y_d = f4(x_d, a0, a1, a2, a3, a4)
+        y_f = f4(x_n, a0, a1, a2, a3, a4)
+        if (y_f * y_d > 0):
+            x_d = x_n
+        else:
+            x_f = x_n
 
 def f4(x, a0, a1, a2, a3, a4):
-	return (a4 * pow(x, 4) + a3 * pow(x, 3) + a2 * pow(x, 2) + a1 * x + a0)
+    return (a4 * pow(x, 4) + a3 * pow(x, 3) + a2 * pow(x, 2) + a1 * x + a0)
--- newton.py
+++ newton.py
@@ -1,15 +1,15 @@
 from bisection import *
 
 def newton(a0, a1, a2, a3, a4, n):
-	x_n = 0.5
-	x = x_n - (f4(x_n, a0, a1, a2, a3, a4)) / derived(x_n, a0, a1, a2, a3, a4)
-	print('%s' %(str(round(x_n, n)).rstrip('0')[:n + 2]))
-	while (True):
-		x = x_n - (f4(x_n, a0, a1, a2, a3, a4) / derived(x_n, a0, a1, a2, a3, a4))
-		if (round(x_n * pow(10, n)) == round(x * pow(10, n))):
-			break
-		x_n = x
-		print('%s' %(str(round(x_n, n)).rstrip('0')[:n + 2]))
+    x_n = 0.5
+    x = x_n - (f4(x_n, a0, a1, a2, a3, a4)) / derived(x_n, a0, a1, a2, a3, a4)
+    print('x = %s' %(str(round(x_n, n)).rstrip('0')[:n + 2]))
+    while (True):
+        x = x_n - (f4(x_n, a0, a1, a2, a3, a4) / derived(x_n, a0, a1, a2, a3, a4))
+        if (round(x_n * pow(10, n)) == round(x * pow(10, n))):
+            break
+        x_n = x
+        print('x = %s' %(str(round(x_n, n)).rstrip('0')[:n + 2]))
 
 def derived(x, a0, a1, a2, a3, a4):
-	return (4 * (a4 * pow(x, 3)) + 3 * (a3 * pow(x, 2)) + 2 * (a2 * pow(x, 1)) + a1)
+    return (4 * (a4 * pow(x, 3)) + 3 * (a3 * pow(x, 2)) + 2 * (a2 * pow(x, 1)) + a1)
